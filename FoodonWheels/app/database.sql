ec5d13d2cbce5119a06feb83fda74b12f1b7e264
Chezl <ezlvansa_myxc002@yahoo.com>
2017-12-04 16:20:43 +0800

    added database

---
+++ FoodonWheels/app/database.sql
@@ -0,0 +1,19 @@
+
+
+-- ############################   TABLES   ############################
+
+
+-- Create the User table
+create table "User"
+(
+  first_name text primary key,
+  last_name text,
+  middle_initial text,
+  email text unique,
+  password text,
+  is_authenticated boolean default True,
+  is_active boolean default True,
+  is_anonymous boolean default True,
+  role text
+);
+
